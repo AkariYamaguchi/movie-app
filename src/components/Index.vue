42f29b1d0abbdb6de7f8ac7e87e76ff4acfd93af
山口明李 <akr03xxx@gmail.com>
2020-08-11 17:20:48 +0900

    リファク（インデント修正）

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,16 +1,16 @@
 <template>
 <div>
-    <h1>Movie app</h1>
-    <div class='container'>
-        <Results title='this is movies..'/>
-        <div v-for="(movie, index) in movies" :key="index">
-        <div class='wrapper_style'>
-            <div class="flex">
-                <div class='title_style'><h1 class='title'>{{ movie.Title }}</h1></div>
-                <div class='img_style'><img class='img' :src="movie.Poster "/></div>
-            </div>
+  <h1>Movie app</h1>
+  <div class='container'>
+    <Results title='this is movies..'/>
+    <div v-for="(movie, index) in movies" :key="index">
+      <div class='wrapper_style'>
+        <div class="flex">
+          <div class='title_style'><h1 class='title'>{{ movie.Title }}</h1></div>
+            <div class='img_style'><img class='img' :src="movie.Poster "/></div>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
 </div>
 </template>
@@ -21,37 +21,36 @@ import Results from '../components/Results'
 const apiurl = "http://www.omdbapi.com/?s=mummy&apikey=ae4b01b0&page=1&type=movie&Content-Type=application/json";
 
 export default {
-    components: {
-        Results,
-    },
-    data(){
-        return{
-            movies: ''
-        }
-    },
-    mounted(){
-        var self = this;
-        axios.get(apiurl).then(function(res) {
-            self.movies = res.data.Search;
-            console.log(self.movies);
-        })
-    },
-
+  components: {
+    Results,
+  },
+  data(){
+    return{
+       movies: ''
+    }
+   },
+  mounted(){
+    var self = this;
+    axios.get(apiurl).then(function(res) {
+    self.movies = res.data.Search;
+    console.log(self.movies);
+    })
+  },
 }
 </script>
 
 <style scoped>
 .container{
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    gap: 30px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 30px;
 }
 .wrapper_style{
-    border: solid 1px #000;
+  border: solid 1px #000;
 }
 /* .flex{
-    display: grid;
-    grid-template-columns: 2fr 1fr;
-    height: 100%;
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  height: 100%;
 } */
 </style>
